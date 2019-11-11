455e2333af73d4db05a231f11911fc36147f06e3
Vinit Mohata <vinit.mohata@gmail.com>
2019-11-11 11:20:03 +0530

    updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-
+var port = process.env.PORT || 3000;
 var captchatext='';
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -29,6 +29,6 @@ app.post('/validate', function (req, res) {
       res.send(false);
 });
 
-app.listen(3000);
+app.listen(port);
 
 console.log("Running at Port 3000");
